f9e879d7f358faaf481fa44d5f408928bf5becb5
Pushpa <pushpavelpula630@gmail.com>
2026-02-04 16:27:47 +0530

    Express

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -1,19 +1,29 @@
-let users= [];
-let id=1;
-
-exports.createUser = (req, res) => {
-    const { name, email} = req.body;
-
-    const newUser = { 
-        id: id++,
+// let users= [];
+// let id=1;
+const User=require('../models/user');
+exports.createUser = async(req, res) => {
+    try {
+        const { name, email} = req.body;
+        const exist= await User.findOne({email});
+        if(exist){
+            return res.status(401).json({message:"User already exists"});
+        }
+    const user = await User.create({
         name,
         email
-    };
-    users.push(newUser);
-    res.status(200).json(newUser);
+    });
+    res.status(200).json(user);
+    } catch (err) {
+        res.status(500).send(err);
+    }
 }
-exports.getUser = (req, res) => {
-    res.status(200).json(users);
+exports.getUser = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).send(err);
+    }
 }
 exports.getUserById = (req, res) => {
     const user=users.find((u) =>u.id === parseInt(req.params.id));
