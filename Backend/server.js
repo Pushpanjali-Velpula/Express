91d8164adef22b9718e14e5456e9c6097c7f55ac
Pushpa <pushpavelpula630@gmail.com>
2026-02-05 10:05:03 +0530

    Backend Api Created

--- Backend/server.js
+++ Backend/server.js
@@ -3,9 +3,11 @@ const todoRoute=require('./router/todoRouter');
 const dotenv=require('dotenv');
 const userRoute=require('./router/userRouter');
 const connectDB=require('./config/db');
+const cors=require('cors');
 
 dotenv.config();
 const app=express();
+app.use(cors());
 connectDB();
 
 app.use(express.json());
